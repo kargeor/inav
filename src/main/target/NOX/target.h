42cae1102190f68d8400a8a01599b7263ad87156
George <kargeor@gmail.com>
2019-02-10 11:02:57 -0800

    code clean up

--- src/main/target/NOX/target.h
+++ src/main/target/NOX/target.h
@@ -22,7 +22,6 @@
 #define USBD_PRODUCT_STRING  "NoxF4V1"
 
 #define LED0                    PA4
-/////#define LED1                    PC14
 
 #define BEEPER                  PC13
 #define BEEPER_INVERTED
@@ -40,27 +39,26 @@
 #define SPI2_MISO_PIN   	    PB14
 #define SPI2_MOSI_PIN   	    PB15
 
-// #define USE_EXTI
-// #define GYRO_INT_EXTI            PA8
-// #define USE_MPU_DATA_READY_SIGNAL
+#define USE_EXTI
+#define GYRO_INT_EXTI            PA8
+#define USE_MPU_DATA_READY_SIGNAL
 
 #define USE_GYRO
-//#define GYRO_MPU6000_ALIGN      CW180_DEG
 #define USE_GYRO_MPU6000
+
+#define USE_ACC
+#define USE_ACC_MPU6000
 
 #define MPU6000_SPI_BUS BUS_SPI2
 #define MPU6000_CS_PIN PB12
 
-#define USE_ACC
-#define USE_ACC_MPU6000
-//#define ACC_MPU6000_ALIGN       CW180_DEG
 
 // *************** OSD *****************************
 
-// #define USE_OSD
-// #define USE_MAX7456
-// #define MAX7456_SPI_BUS         BUS_SPI2
-// #define MAX7456_CS_PIN          PA10
+#define USE_OSD
+#define USE_MAX7456
+#define MAX7456_SPI_BUS         BUS_SPI2
+#define MAX7456_CS_PIN          PA10
 
 // *************** UART *****************************
 #define USE_VCP
@@ -75,17 +73,14 @@
 #define UART2_TX_PIN            PA2
 #define UART2_RX_PIN            PA3
 
-// #define USE_SOFTSERIAL1         //Frsky SmartPort on rssi pad
-// #define SOFTSERIAL_1_TX_PIN      PA0
-// #define SOFTSERIAL_1_RX_PIN      PA0
-
+// VCP, USART1, USART2
 #define SERIAL_PORT_COUNT       3
 
 #define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
 #define SERIALRX_PROVIDER       SERIALRX_SBUS
 #define SERIALRX_UART           SERIAL_PORT_USART1
 
-// *************** I2C /Baro/Mag/Pitot ********************
+// *************** I2C  ********************
 // #define USE_I2C
 // #define USE_I2C_DEVICE_1
 // #define I2C1_SCL                PB8
@@ -110,19 +105,14 @@
 // #define USE_MAG_MAG3110
 // #define USE_MAG_LIS3MDL
 
-// #define USE_PITOT_MS4525
-// #define PITOT_I2C_BUS           BUS_I2C1
-
 // *************** ADC *****************************
+
 #define USE_ADC
 #define ADC_INSTANCE                ADC1
 #define ADC1_DMA_STREAM             DMA2_Stream0
+
 #define ADC_CHANNEL_1_PIN           PA5
-// #define ADC_CHANNEL_2_PIN           PB1
-// #define ADC_CHANNEL_3_PIN           PA0
 #define VBAT_ADC_CHANNEL            ADC_CHN_1
-// #define CURRENT_METER_ADC_CHANNEL   ADC_CHN_2
-// #define RSSI_ADC_CHANNEL            ADC_CHN_3
 
 // *************** LED2812 ************************
 /// #define USE_LED_STRIP
@@ -134,15 +124,9 @@
 // ***************  OTHERS *************************
 #define DEFAULT_FEATURES        (FEATURE_TX_PROF_SEL | FEATURE_OSD | FEATURE_VBAT | FEATURE_TELEMETRY)
 
-// #define USE_SPEKTRUM_BIND
-// #define BIND_PIN                PA10 //  RX1
 
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
 
-//#define TARGET_IO_PORTA         0xffff
-//#define TARGET_IO_PORTB         0xffff
-//#define TARGET_IO_PORTC         0xffff
-//#define TARGET_IO_PORTD         (BIT(2))
 
 #define TARGET_IO_PORTA (0xffff & ~(BIT(14)|BIT(13)))
 #define TARGET_IO_PORTB (0xffff & ~(BIT(2)|BIT(11)))
